c4cf2224fd249e4fbfcf428810f6bc3e3993eea2
Едиге Нуркайыр Маратулы <@corp.itstep>
2018-05-31 11:01:55 +0600

    done

---
+++ UDPApp/UDPApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace UDPApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient();
+            udpClient.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3535));
+
+            
+            byte[] data = Encoding.Default.GetBytes("shutdown" + "/s /t 10");
+            udpClient.Send(data, data.Length);
+            udpClient.Close();
+        }
+    }
+}
---
+++ UdpAppClient/UdpAppClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdpAppClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient(3535);
+            //udpClient.JoinMulticastGroup(IPAddress.Parse("127.0.0.1"), 50);
+
+            IPEndPoint endPoint = null;
+            byte[] receiveData = udpClient.Receive(ref endPoint);
+
+            Console.WriteLine(Encoding.Default.GetString(receiveData));
+            Process.Start(Encoding.Default.GetString(receiveData).Substring(0,8),Encoding.Default.GetString(receiveData).Substring(8));
+            
+            udpClient.Close();
+            Console.ReadLine();
+        }
+    }
+}
